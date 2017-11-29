af22dc8b211960e91118d959a6162cf2a6fd2067
Jeferson Fiuza Passos <jeferson.passosf@gmail.com>
2017-11-29 13:55:34 -0200

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,12 +1,12 @@
 class Config(object):
     DEBUG = True
     TESTING = True
-    DATABASE_URI = 'performance/performance@perfp'
+    DATABASE_URI = 'user/pass@perfp'
     APPLICATION_ROOT = '/api/sms'
     MS_HOST = "10.58.0.222"
-    MS_DB = "FRAMEWORK"
-    MS_USER = "FRAMEWORK"
-    MS_PASS = "conecta@2015"
+    MS_DB = "database"
+    MS_USER = "user"
+    MS_PASS = "pass"
 
 class Production(Config):
     DEBUG = False
